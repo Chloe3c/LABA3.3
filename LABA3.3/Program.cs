8a8a299d9276ef8260c0dd00f9ef25b2159a309e
Chloe3c <kristianishenko21@gmail.com>
2024-11-11 21:03:23 +0200

    Добавьте файлы проекта.

---
+++ LABA3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("введите количество членов (n):");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("введите значение x:");
+        double x = double.Parse(Console.ReadLine());
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            double term = (1 + 2 * (i - 1)) * Math.Pow((x / 2), i) / Factorial(i);
+            sum += term;
+        }
+        Console.WriteLine("сумма: " + sum);
+    }
+    static double Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
